5242b8df4af05d8e2078adb46abe57c8bffb5021
Heerkog <heerkogroefsema@gmail.com>
2018-05-24 11:48:32 +0200

    ConditionalM

--- src/nl/rug/ds/bpm/net/marking/ConditionalM.java
+++ src/nl/rug/ds/bpm/net/marking/ConditionalM.java
@@ -3,10 +3,15 @@ package nl.rug.ds.bpm.net.marking;
 import nl.rug.ds.bpm.expression.Expression;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 /**
  * Created by Heerko Groefsema on 14-May-18.
  */
 public interface ConditionalM extends M {
+	void setConditions(HashMap<String, Expression<?>> conditions);
 	Collection<Expression<?>> getConditions();
+
+	void addCondition(String condition);
+	void removeCondition(String condition);
 }
